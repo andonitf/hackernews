8e69318a23b5658fe13ccf3cde261e10e8491a44
Andoni Torres <andonitf@gmail.com>
2018-11-29 16:58:56 +0100

    Composeable Components

--- src/App.js
+++ src/App.js
@@ -69,7 +69,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Search value={searchTerm} onChange={this.onSearchChange}>
+          Search
+        </Search>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
@@ -78,9 +80,10 @@ class App extends Component {
 
 class Search extends Component {
   render() {
-    const { value, onChange } = this.props;
+    const { value, onChange, children } = this.props;
     return (
       <form>
+        {children}
         <input type="text" value={value} onChange={onChange} />
       </form>
     );
