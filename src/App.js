aeee1f848343e2cefd063e3b024e274a6c17309a
Andoni Torres <andonitf@gmail.com>
2018-11-30 12:00:05 +0100

    Getting Real with an API

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React, { Component } from "react";
 // import logo from './logo.svg';
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://facebook.github.io/react/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
+const DEFAULT_QUERY = "redux";
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+// console.log(url);
+// const list = [
+//   {
+//     title: "React",
+//     url: "https://facebook.github.io/react/",
+//     author: "Jordan Walke",
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0
+//   },
+//   {
+//     title: "Redux",
+//     url: "https://github.com/reactjs/redux",
+//     author: "Dan Abramov, Andrew Clark",
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1
+//   }
+// ];
 
 // // ES5
 // function isSearched(searchTerm) {
@@ -34,19 +40,45 @@ const list = [
 const isSearched = searchTerm => item =>
   !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
+const largeColumn = {
+  width: "40%"
+};
+const midColumn = {
+  width: "30%"
+};
+const smallColumn = {
+  width: "10%"
+};
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: ""
+      result: null,
+      searchTerm: DEFAULT_QUERY
     };
 
+    this.setSearchTopstories = this.setSearchTopstories.bind(this);
+    this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
 
+  setSearchTopstories(result) {
+    console.log('result');
+    console.log(result);
+    
+    this.setState({ result });
+  }
+  fetchSearchTopstories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopstories(result));
+  }
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopstories(searchTerm);
+  }
   onDismiss(id) {
     // function isNotId(item) {
     //   return item.objectID !== id;
@@ -65,8 +97,8 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
-
+    const { searchTerm, result } = this.state;
+    if (!result) { return null; }
     return (
       <div className="page">
         <div className="interactions">
@@ -74,7 +106,7 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
   }
@@ -86,16 +118,6 @@ const Search = ({ value, onChange, children }) => (
     <input type="text" value={value} onChange={onChange} />
   </form>
 );
-
-const largeColumn = {
-  width: "40%"
-};
-const midColumn = {
-  width: "30%"
-};
-const smallColumn = {
-  width: "10%"
-};
 
 const Table = ({ list, pattern, onDismiss }) => (
   <div className="table">
